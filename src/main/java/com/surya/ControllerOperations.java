1ebae17403e2ab14a52bc97e044b33952478cb04
SURYA-TEJA-BTECH <isurya834@gmail.com>
2025-06-29 14:52:18 +0530

    first commit

---
+++ src/main/java/com/surya/ControllerOperations.java
@@ -0,0 +1,15 @@
+package com.surya;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class ControllerOperations {
+
+	@GetMapping("/msg")
+	public String getMethodName(@RequestParam String name) {
+		return "Good Morning " + name;
+	}
+
+}
